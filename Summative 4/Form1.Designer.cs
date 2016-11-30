dd73a0578c75c0ceac5f445ea32eb744878fd947
addiepant <aadhpant936@ed.amdsb.ca>
2016-11-30 15:14:15 -0500

    Add project files.

---
+++ Summative 4/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Summative_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.outputLabel.Location = new System.Drawing.Point(257, 176);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(287, 40);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "Open Mission Statement";
+            // 
+            // startButton
+            // 
+            this.startButton.BackgroundImage = global::Summative_4.Properties.Resources.background;
+            this.startButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.GhostWhite;
+            this.startButton.Location = new System.Drawing.Point(329, 260);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(130, 70);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(822, 484);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.outputLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mission";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ Summative 4/Form1.cs
@@ -0,0 +1,95 @@
+﻿// Addie Pant
+// November 25, 2016
+// Santa Claus Christmas Delievery thingy
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Summative_4
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Pen chimneyPen = new Pen(Color.Red, 10);
+            SolidBrush giftBrush = new SolidBrush(Color.Snow);
+            SolidBrush flameBrush = new SolidBrush(Color.Orange);
+            SolidBrush lightBrush = new SolidBrush(Color.IndianRed);
+            SolidBrush slayBrush = new SolidBrush(Color.SaddleBrown);
+  
+            int x = 422; 
+            Graphics fg = this.CreateGraphics();
+
+            //when button is pressed change background colour to Black and make outputLabel and startButton invisble
+            this.BackColor = Color.Black;
+            outputLabel.Visible = false;
+            startButton.Visible = false;
+
+            //Mission statement
+            Font drawFont = new Font("Arial", 12, FontStyle.Bold);
+            SolidBrush drawBrush = new SolidBrush(Color.Red);
+            fg.DrawString("Mission Statement: Drop Santas Christmas Present in the chimney when the fire's lit", drawFont, drawBrush, 0, 140);
+            Thread.Sleep(5000);
+           
+
+            //sleigh and chimney
+            for (int i = 0; i < 423; i++)
+             {
+                fg.Clear(Color.Black);
+                fg.DrawLine(chimneyPen, 0, 260, 355, 260);
+                fg.DrawLine(chimneyPen, 350, 260, 350, 177);
+                fg.DrawLine(chimneyPen, 345, 177, 500, 177);
+                fg.DrawLine(chimneyPen, 495, 177, 495, 260);
+                fg.DrawLine(chimneyPen, 490, 260, this.Width, 260);
+
+                fg.FillEllipse(slayBrush, i, 5, 50, 25);
+
+                Thread.Sleep(11);
+              }
+
+            // chimeny and gift
+            for (int y = 0; y < 300; y++)
+             {
+                fg.Clear(Color.Black);
+                fg.FillEllipse(slayBrush, x, 1, 50, 25);
+                fg.DrawLine(chimneyPen, 0, 260, 355, 260);
+                fg.DrawLine(chimneyPen, 350, 260, 350, 177);
+                fg.DrawLine(chimneyPen, 345, 177, 500, 177);
+                fg.DrawLine(chimneyPen, 495, 177, 495, 260);
+                fg.DrawLine(chimneyPen, 490, 260, this.Width, 260);
+
+                fg.FillRectangle(giftBrush, x, y, 10, 10);
+
+                Thread.Sleep(11);
+               }
+
+            //gift explosion
+              for (int pixels = 1; pixels < 101; pixels++)
+
+               {
+                fg.FillEllipse(flameBrush, x - pixels / 2, 300 - pixels / 2, 10 + pixels, 10 + pixels);
+                Thread.Sleep(10);
+                fg.FillEllipse(lightBrush, x - pixels / 2, 300 - pixels / 2, 10 + pixels, 10 + pixels);
+                Thread.Sleep(10);
+                fg.FillEllipse(giftBrush, x - pixels / 2, 300 - pixels / 2, 10 + pixels, 10 + pixels);
+                Thread.Sleep(10);
+                }
+        }
+
+    }
+}
+
